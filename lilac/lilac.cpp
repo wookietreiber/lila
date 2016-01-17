b323a6c61b17db07efc7454e265469a0640178dc
Christian Krause <kizkizzbangbang@googlemail.com>
2016-01-17 19:12:35 +0100

    generating native .o files, just need to be linked

--- lilac/lilac.cpp
+++ lilac/lilac.cpp
@@ -9,9 +9,14 @@
 #include <getopt.h>
 #include <iostream>
 
-#include <llvm/Support/raw_ostream.h>
+#include <llvm/Analysis/TargetLibraryInfo.h>
+#include <llvm/IR/LegacyPassManager.h>
 #include <llvm/Support/FileSystem.h>
-#include <llvm/Bitcode/ReaderWriter.h>
+#include <llvm/Support/TargetRegistry.h>
+#include <llvm/Support/TargetSelect.h>
+#include <llvm/Support/ToolOutputFile.h>
+#include <llvm/Target/TargetMachine.h>
+#include <llvm/Target/TargetSubtargetInfo.h>
 
 #ifdef HAVE_CONFIG_H
 #include "config.h"
@@ -26,7 +31,7 @@ using namespace lila::parser;
 int main(int argc, char** argv) {
 
   const char *input = "-";
-  const char *output = "a.out.bc";
+  const char *output = "a.out.o";
 
   bool verbose = false;
 
@@ -127,18 +132,93 @@ int main(int argc, char** argv) {
   codegen.generateCode(move(ast));
 
   // ---------------------------------------------------------------------------
-  // write LLVM IR code
+  // write object file
   // ---------------------------------------------------------------------------
 
-  std::error_code ec;
-  llvm::raw_fd_ostream out(output, ec, llvm::sys::fs::F_None);
+  llvm::InitializeAllTargets();
+  llvm::InitializeAllTargetMCs();
+  llvm::InitializeAllAsmPrinters();
+  llvm::InitializeAllAsmParsers();
 
-  if (ec) {
-    cerr << "error: " << output << ": " << ec.message() << endl;
-    return ec.value();
+  llvm::PassRegistry *Registry = llvm::PassRegistry::getPassRegistry();
+  llvm::initializeCore(*Registry);
+  llvm::initializeCodeGen(*Registry);
+  llvm::initializeLoopStrengthReducePass(*Registry);
+  llvm::initializeLowerIntrinsicsPass(*Registry);
+  llvm::initializeUnreachableBlockElimPass(*Registry);
+
+  llvm::Triple TheTriple(llvm::sys::getDefaultTargetTriple());
+
+  std::string Error;
+  const llvm::Target *TheTarget =
+    llvm::TargetRegistry::lookupTarget(TheTriple.getTriple(), Error);
+
+  if (!TheTarget) {
+    cerr << Error << endl;
+    return 1;
   }
 
-  llvm::WriteBitcodeToFile(codegen.module.get(), out);
+  // compile for native cpu
+  std::string CPUStr = llvm::sys::getHostCPUName();
+
+  // compile for native cpu features
+  llvm::SubtargetFeatures Features;
+  llvm::StringMap<bool> HostFeatures;
+  if (llvm::sys::getHostCPUFeatures(HostFeatures))
+    for (auto &F : HostFeatures)
+      Features.AddFeature(F.first(), F.second);
+  std::string FeaturesStr = Features.getString();
+
+  llvm::CodeGenOpt::Level OLvl = llvm::CodeGenOpt::Default;
+
+  llvm::TargetOptions Options;
+
+  std::unique_ptr<llvm::TargetMachine> Target(
+    TheTarget->createTargetMachine(TheTriple.getTriple(),
+                                   CPUStr,
+                                   FeaturesStr,
+                                   Options,
+                                   llvm::Reloc::Default,
+                                   llvm::CodeModel::Default,
+                                   OLvl));
+
+  std::error_code EC;
+  auto Out = llvm::make_unique<llvm::tool_output_file>(output, EC, llvm::sys::fs::F_None);
+
+  llvm::legacy::PassManager PM;
+  llvm::TargetLibraryInfoImpl TLII(TheTriple);
+  PM.add(new llvm::TargetLibraryInfoWrapperPass(TLII));
+
+  if (const llvm::DataLayout *DL = Target->getDataLayout())
+    codegen.module->setDataLayout(*DL);
+
+  // add features to functions of module
+  for (auto &F : *codegen.module) {
+    auto &Ctx = F.getContext();
+    llvm::AttributeSet Attrs = F.getAttributes(), NewAttrs;
+
+    if (!CPUStr.empty())
+      NewAttrs = NewAttrs.addAttribute(Ctx, llvm::AttributeSet::FunctionIndex,
+                                       "target-cpu", CPUStr);
+
+    if (!FeaturesStr.empty())
+      NewAttrs = NewAttrs.addAttribute(Ctx, llvm::AttributeSet::FunctionIndex,
+                                       "target-features", FeaturesStr);
+
+    NewAttrs = Attrs.addAttributes(Ctx, llvm::AttributeSet::FunctionIndex, NewAttrs);
+    F.setAttributes(NewAttrs);
+  }
+
+  llvm::raw_pwrite_stream *OS = &Out->os();
+
+  if (Target->addPassesToEmitFile(PM, *OS, llvm::TargetMachine::CGFT_ObjectFile)) {
+    cerr << "error: target does not support generation of this file type" << endl;
+    return 1;
+  }
+
+  PM.run(*codegen.module);
+
+  Out->keep();
 
   // ---------------------------------------------------------------------------
   // end
