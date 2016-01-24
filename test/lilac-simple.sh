33a8e2cf48aa34a84d9b47858ab259d82a52111b
Christian Krause <kizkizzbangbang@googlemail.com>
2016-01-24 13:28:41 +0100

    add simple lilac compilation test

---
+++ test/lilac-simple.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+LILAC=../lilac/lilac
+
+OBJECT=$(mktemp)
+LINKED=$(mktemp)
+
+trap "rm -f $OBJECT $LINKED" EXIT
+
+$LILAC -v -o $OBJECT <<< '20 + 22'
+# TODO remove explicit clang usage
+clang -o $LINKED $OBJECT
+
+RESULT=$($LINKED)
+
+if [[ $RESULT == "42.000000" ]] ; then
+  echo "result matches expected value"
+  exit 0
+else
+  echo "result did not match expected value ($RESULT != 42)"
+  exit 1
+fi
