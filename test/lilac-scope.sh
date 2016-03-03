f5ca57bc1edc37d8d9209228cbc3f93815605b95
Christian Krause <kizkizzbangbang@googlemail.com>
2016-03-03 23:23:17 +0100

    more evolved scoping example

--- test/lilac-scope.sh
+++ test/lilac-scope.sh
@@ -6,9 +6,21 @@ cat << EOF | test_compilation "42.000000"
 val a = 21
 
 val b = {
-  val a = 2
-  a * 2
+  val a = 1
+  a * 3
 }
 
-a * 2
+def foo(a,b) = {
+  def bar(a,b) = {
+    def baz(a,b) = {
+      a * b
+    }
+
+    baz(a, b + 2)
+  }
+
+  bar(a, b + 2)
+}
+
+foo(3, 2) + a + b
 EOF
