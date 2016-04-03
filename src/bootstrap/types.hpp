59786b57b953f39f3d36eaad7f702cb7cd0fadec
Christian Krause <kizkizzbangbang@googlemail.com>
2016-04-03 13:14:22 +0200

    foo

---
+++ src/bootstrap/types.hpp
@@ -0,0 +1,25 @@
+/*            __ __                                                     *\
+**     __    /_// /  ___            lila bootstrap compiler             **
+**    / /   __ / /  / _ |           (c) 2016, Christian Krause          **
+**   / /__ / // /__/ __ |                                               **
+**  /____//_//____/_/ | |                                               **
+\*                    |/                                                */
+
+#ifndef LILA_TYPES_H
+#define LILA_TYPES_H
+
+using namespace std;
+
+namespace lila {
+  namespace types {
+
+    class AnyType {
+    };
+
+    class DoubleType : public AnyType {
+    };
+
+  }
+}
+
+#endif
